e62d92ea96fb3b3a3692694bb5d4c26a7af37960
IshaYoo <73917514+IshaYoo@users.noreply.github.com>
2021-12-08 16:59:08 -0500

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import tkinter as tk
 from tkinter import *
 from tkinter import filedialog, Text, messagebox
 from graph import *
-#E48214 is the color gator orange
+#1DB954 is the Spotify green color
 
 
 
@@ -11,8 +11,6 @@ G = Graph()
 def runProgram():
     #Make graph
     #Check if new songs have been inserted, make new graph if needed
-    textBoxDij.config(text = "Searching for connection...")
-    textBoxBFS.config(text = "Searching for connection...")
 
     song1 = chooseSong1.get(1.0, "end-1c")
     artist1 = chooseArtist1.get(1.0, "end-1c")
@@ -28,24 +26,24 @@ def runProgram():
 #Creating main root of the GUI
 root = tk.Tk()
 root.title("Spotify Song Linker")
-root.geometry("650x800")
-root.configure(bg = '#E48214') #Setting background color
-titleLabel = Label(root, text = "Spotify Song Linker", bg = '#E48214', font=("Helvetica", 25)).place(x = 200, y = 30)
+root.geometry("650x700")
+root.configure(bg = '#1DB954') #Setting background color
+titleLabel = Label(root, text = "Spotify Song Linker", bg = '#1DB954', font=("Helvetica", 25)).place(x = 200, y = 30)
 
 ###########################################################################
 
 #Creating button ONE
 #songOneVar = string(root) #Song option variable
-song1Label = Label(root, text = "Song choice 1", bg = '#E48214').place(x = 20, y = 80)
+song1Label = Label(root, text = "Song choice 1", bg = '#1DB954').place(x = 20, y = 80)
 chooseSong1 = tk.Text(root, height = 2, width = 20)
-chooseSong1.config(font = ('Helvetica', 12), fg = 'orange')
+chooseSong1.config(font = ('Helvetica', 12))
 chooseSong1.place(x = 20, y = 100)
 
 #Artist ONE
 artistOneVar = str(root) #Artist option variable
-artist1Label = Label(root, text = "Artist choice 1", bg = '#E48214').place(x = 20, y = 150)
+artist1Label = Label(root, text = "Artist choice 1", bg = '#1DB954').place(x = 20, y = 150)
 chooseArtist1 = tk.Text(root, height = 2, width = 20)
-chooseArtist1.config(font = ('Helvetica', 12), fg = 'orange')
+chooseArtist1.config(font = ('Helvetica', 12))
 chooseArtist1.place(x = 20, y = 170)
 
 ###########################################################################
@@ -53,15 +51,15 @@ chooseArtist1.place(x = 20, y = 170)
 
 #Creating button TWO
 songTwoVar = str(root) #Song option variable
-song2Label = Label(root, text = "Song choice 2", bg = '#E48214').place(x = 460, y = 80)
+song2Label = Label(root, text = "Song choice 2", bg = '#1DB954').place(x = 460, y = 80)
 chooseSong2 = tk.Text(root, height = 2, width = 20)
-chooseSong2.config(font = ('Helvetica', 12), fg = 'orange')
+chooseSong2.config(font = ('Helvetica', 12))
 chooseSong2.place(x = 460, y = 100)
 #Artist TWO
 artistTwoVar = str(root) #Artist option variable
-artist2Label = Label(root, text = "Artist choice 2", bg = '#E48214').place(x = 460, y = 150)
+artist2Label = Label(root, text = "Artist choice 2", bg = '#1DB954').place(x = 460, y = 150)
 chooseArtist2 = tk.Text(root, height = 2, width = 20)
-chooseArtist2.config(font = ('Helvetica', 12), fg = 'orange')
+chooseArtist2.config(font = ('Helvetica', 12))
 chooseArtist2.place(x = 460, y = 170)
 
 
@@ -69,17 +67,18 @@ chooseArtist2.place(x = 460, y = 170)
 
 
 
-labelDij = Label(root, text = "Results from Dijkstra’s Algorithm", bg = '#E48214').place(x = 28, y = 300)
-textBoxDij = Label(root, height = 8, width = 40)
-textBoxDij.place(relx = 0.05, rely = 0.72)
+labelDij = Label(root, text = "Results from Dijkstra’s Algorithm", bg = '#1DB954').place(x = 45, y = 300)
+textBoxDij = Label(root, height = 20, width = 25)
+textBoxDij.place(relx = 0.06, rely = 0.5)
+
 
 
 mkGraph = tk.Button(root, text = "Make and Traverse", command = runProgram).place(x = 240, y = 250)
 
 
-labelBFS = Label(root, text = "Results from Breadth First Search", bg = '#E48214').place(x = 345, y = 300)
-textBoxBFS = Label(root, height = 5, width = 20)
-textBoxBFS.place(relx = 0.57, rely = 0.72)
+labelBFS = Label(root, text = "Results from Breadth First Search", bg = '#1DB954').place(x = 370, y = 300)
+textBoxBFS = Label(root, height = 20, width = 25)
+textBoxBFS.place(relx = 0.56, rely = 0.5)
 
 
 
